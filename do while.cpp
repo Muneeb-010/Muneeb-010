4536c7e12e4f30193acd042b212aebd515fbb46c
Muneeb-010 <87256858+Muneeb-010@users.noreply.github.com>
2021-07-19 06:49:52 -0700

    Add files via upload

---
+++ do while.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int i=0,marks;
+do
+{
+cout<<"marks are"<<endl;
+	int marks[]={23,34,45,65};
+	cout<<"your marks are "<<i<<marks[i]<<endl;
+}
+while(marks[]=i)
+;{
+	
+}
+};
+
